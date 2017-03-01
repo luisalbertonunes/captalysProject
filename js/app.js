1e5e7ecfafaa780c81c0a8501662bd8cee6caadb
luisalbertonunes <luisnunes.al@gmail.com>
2017-03-01 11:36:22 -0300

    correçoes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-$('.animais .tab-menu a').click(function(e){
-	e.preventDefault();
-	var itemId=$(this).attr('href') //pegando o valor do atributo href de onde eu cliquei
-	$('.animais .tab-menu a, .animais .item').removeClass('active'); //removo a class active de todos os .tab-menu a e class item
-	$(this).addClass('active'); //add no class active no que eu cliquei
-	$(itemId).addClass('active'); //add no href do item que eu cliquei
+$('.mobile-btn').click(function(){
+	$(this).toggleClass('active');
+	$('mobile-menu').toggleClass('active');
 });
-
