304309060c12c2871da919220d03110d4eeccb2f
luisalbertonunes <luisnunes.al@gmail.com>
2017-03-01 12:20:53 -0300

    animação scroll

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+//botao mobile
 $('.mobile-btn').click(function(){
 	$(this).toggleClass('active');
+});
+
+//scroll suave menu
+$('.navbar-animate a[href^="#"]').click(function(e){
+	e.preventDefault();
+	
+	var id = $(this).attr('href'),
+		menuHeight = $('.navbar-animate').innerHeight(),
+		targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+
+// Scroll suave para o topo
+$('.logo').click(function(e){
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: 0
+	}, 500)
 });
