a2e3f127de767b65aa729f46a03b473b047b9efa
luisalbertonunes <luisnunes.al@gmail.com>
2017-03-01 12:44:24 -0300

    animacao ao scrollar

--- js/app.js
+++ js/app.js
@@ -23,3 +23,12 @@ $('.logo').click(function(e){
 		scrollTop: 0
 	}, 500)
 });
+
+$(window).scroll(function() {
+	var windowTop = $(this).scrollTop();
+	$('.anime').each(function(){
+		if(windowTop > $(this).offset().top - 400) {
+			$(this).addClass('anime-init');
+		}
+	});
+});
